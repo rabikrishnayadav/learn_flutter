daf3d49798d804e655866bb8f7d9bc4662b980d4
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-10-10 08:45:24 +0530

    Loops in Dart

---
+++ lib/language_files/loops.dart
@@ -0,0 +1,16 @@
+// loop is used for repeat the code till condition will true and exit from loop when condition will false
+
+main(){
+
+  // for loop
+  for (int i = 0; i <= 9; i++){
+    print("Number: $i");
+  }
+
+  // while loop
+  int j = 0;
+  while(j <= 5){
+    print("Number: $j");
+    j++;
+  }
+}
