3d3c3fdf4d82ef3f8332d5537b408214d6f404c5
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-10-10 08:32:23 +0530

    Condition in Dart

---
+++ lib/language_files/Condition.dart
@@ -0,0 +1,17 @@
+
+
+main(){
+
+  var a = 100;
+
+  if(a > 200){
+    print("a is greater 200");
+  }else if( a > 150){
+    print("a is greater 150");
+  }else if( a >= 100){
+    print("a is greater or equal 100");
+  }else{
+    print("a is less than 100");
+  }
+
+}
