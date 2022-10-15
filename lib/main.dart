471560b297ebdb34eb48371a856389a8290f1c7d
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-10-15 12:35:26 +0530

    Splitting the Widget in multiple part

--- lib/main.dart
+++ lib/main.dart
@@ -89,24 +89,131 @@ class _MyHomePageState extends State<MyHomePage> {
 
     var time = DateTime.now();
 
-    var arrColors = [Colors.amber,
-    Colors.orange,
-      Colors.grey,
-      Colors.blue,
-      Colors.pink,
-      Colors.green,
-      Colors.purpleAccent,
-      Colors.brown
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: Text("GridView Widget", style: mTextStyle50()),
       ),
-      body: ElevatedButton(
-          child: Text("Click here"),
-          onPressed: callBack,
+      body: Container(
+        child: Column(
+          children: [
+            CatItems(),
+            Contact(),
+            SubCatItems(),
+            BottomMenu()
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class CatItems extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.blue,
+        child: ListView.builder(itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.all(10),
+          child: SizedBox(
+            width: 100,
+            child: CircleAvatar(
+              backgroundColor: Colors.green,
+            ),
+          ),
+        ),
+          itemCount: 8,
+          scrollDirection: Axis.horizontal,
+        ),
       ),
     );
+  }
+  
+}
+
+class Contact extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        color: Colors.orange,
+        child: ListView.builder(itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.green,
+            ),
+            title: Text("Name"),
+            subtitle: Text("mobile Number"),
+            trailing: Icon(Icons.add),
+          ),
+        ),
+          itemCount: 8,
+        ),
+      ),
+    );
+  }
+}
+
+class SubCatItems extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: Container(
+          color: Colors.blueGrey,
+          child: ListView.builder(itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue
+              ),
+            ),
+          ),
+            itemCount: 8,
+            scrollDirection: Axis.horizontal,
+          ),
+        )
+    );
+  }
+}
+
+class BottomMenu extends StatelessWidget{
+
+  var arrColors = [Colors.amber,
+    Colors.orange,
+    Colors.grey,
+    Colors.blue,
+    Colors.pink,
+    Colors.green,
+    Colors.purpleAccent,
+    Colors.brown
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: Container(
+          color: Colors.green,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.count(
+              crossAxisCount: 4,
+              crossAxisSpacing: 10,
+              children: [
+                Container(color: arrColors[0],),
+                Container(color: arrColors[1],),
+                Container(color: arrColors[2],),
+                Container(color: arrColors[3],),
+              ],
+            ),
+          ),
+        ));
   }
 }
